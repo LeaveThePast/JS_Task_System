256b4efac368fee289ecb21c50e7ae977176e05d
LeaveThePast <Ramil.Budagli@yandex.ru>
2024-02-25 23:14:34 +0400

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
       newCardFormElement.classList.add("show");
 
-      newCardFormSubmitElement.addEventListener("click", () => {
+      newCardFormSubmitElement.addEventListener("submit", (e) => {
+        e.preventDefault();
         const newCardFormInputElementValue =
           newCardFormInputElement.value.trim();
         if (newCardFormInputElementValue) {
@@ -28,48 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const board = new Board();
   board.loadFromLocalStorage(board);
 
-  // Drag & Drop - пожилой
-  // let draggedElement;
-  // const CardContainersAndCardsElements =
-  //   document.querySelectorAll(".cardContainer");
-  // CardContainersAndCardsElements.forEach((container) => {
-  //   container.addEventListener("mousedown", (e) => {
-  //     e.preventDefault();
-  //     draggedElement = e.target;
-  //     draggedElement.classList.add("dragged");
-  //     document.documentElement.addEventListener("mouseup", () =>
-  //       onMouseUp(container)
-  //     );
-  //     document.documentElement.addEventListener("mouseover", onMouseOver);
-  //   });
-  // });
-
-  // const onMouseUp = (container) => (e) => {
-  //   draggedElement.classList.remove("dragged");
-  //   draggedElement = undefined;
-
-  //   document.documentElement.removeEventListener("mouseup", onMouseUp);
-  //   document.documentElement.removeEventListener("mouseover", onMouseOver);
-  //   const draggedElementUp = e.target;
-  //   container.insertBefore(draggedElement, draggedElementUp);
-  // };
-
-  // const onMouseOver = (e) => {
-  //   draggedElement.style.top = e.clientY + "px";
-  //   draggedElement.style.left = e.clientX + "px";
-  // };
-
-  // Drag & Drop - современный
+  // Drag & Drop
   let draggedElement;
   const cardContainers = document.querySelectorAll(".cardContainer");
   cardContainers.forEach((container) => {
     container.addEventListener("dragstart", (e) => {
-      draggedElement = e.target;
-      e.target.classList.add("dragged");
+      draggedElement = e.target.closest(".card");
+      draggedElement.classList.add("dragged");
     });
 
     container.addEventListener("dragend", (e) => {
-      e.target.classList.remove("dragged");
+      draggedElement.classList.remove("dragged");
     });
 
     container.addEventListener("dragover", (e) => {
@@ -78,7 +48,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     container.addEventListener("drop", (e) => {
       e.preventDefault();
-      container.insertBefore(draggedElement, e.target);
+      container.insertBefore(draggedElement, e.target.closest(".card"));
     });
   });
 });
